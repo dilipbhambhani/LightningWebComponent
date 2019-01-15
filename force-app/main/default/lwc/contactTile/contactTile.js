7d372269ba102ef87c40ead246b08dcc4d2ee2cf
unknown <dilipbhambhani1@gmail.com>
2019-01-15 15:35:18 +0530

    LightningWebComponent

---
+++ force-app/main/default/lwc/contactTile/contactTile.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+export default class ContactTile extends NavigationMixin(LightningElement) {
+    @api contact;   
+    navigateToContact() {  
+        this[NavigationMixin.Navigate]({
+            type: "standard__recordPage",
+            attributes: {
+                recordId: this.contact.Id,
+                objectApiName: "Contact",
+                actionName: "view",
+            },
+        });
+    }
+}
